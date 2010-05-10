eeb772c8317ed009b414685c7b0f4ec97d949cfc
James Michael DuPont <mdupont@introspector-mail.(none)>
2010-05-10 14:08:14 +0200

    first version

---
+++ ogr/ogrsf_frmts/dxfdwg/OdFileBuf.cpp
@@ -0,0 +1,3 @@
+#ifdef USE_ODA
+/// removed code that might be infringing on ODA licenses
+#endif // no ODA stuff
---
+++ ogr/ogrsf_frmts/dxfdwg/OdFileBuf.h
@@ -0,0 +1,11 @@
+#ifndef ODFILEBUF_DEFINED
+#define ODFILEBUF_DEFINED
+
+#ifdef USE_ODA
+
+// removed possibly infringing code
+
+#endif // use ODA
+#endif // ODFILEBUF_DEFINED
+
+
